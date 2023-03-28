f47727cc1d2bc5fc401e7998c8a63b64bfd3a003
nightshadow92 <s.allen.hare92@gmail.com>
2023-03-28 14:50:40 -0500

    add files

---
+++ tts.ps1
@@ -0,0 +1,21 @@
+﻿$text = ((Get-Content text.txt) -split "\n" | Where-Object { -not [string]::IsNullOrEmpty($_) -and -not [string]::IsNullOrWhiteSpace($_) })[2]
+
+$models = (tts --list_models | where {$_ -match "\/en\/"} | where {$_ -match "tts_models"}) -replace "\d{1,2}: "
+$vocoder = (tts --list_models | where {$_ -match "\/en\/"} | where {$_ -match "vocoder"}) -replace "\d{1,2}: "
+
+$models | foreach {
+$mod = $_
+tts --model_name $mod --text $text --out_path "$($mod).wav"
+
+}
+
+
+$models | foreach {
+$mod = $_
+$vocoder | foreach {
+$voc = $_
+tts --model_name $mod  --vocoder_name $voc --text $text --out_path "$($mod)-$($voc).wav"
+}
+}
+
+#tts --model_name "tts_models/en/ljspeech/tacotron2-DDC" --vocoder_name "vocoder_models/en/ljspeech/hifigan_v2"  --text $text --out_path "a.wav"
